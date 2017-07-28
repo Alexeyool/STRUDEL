b32f97eda0de6d308fa48356cd27e90db8d1c2f3
Alexeyool <Alexeymmy@gmail.com>
2017-07-28 15:48:42 +0300

    class DownloadCovers Transferring to MainActivity.

--- app/src/main/java/com/strudel/alexeyool/strudel/MainActivity.java
+++ app/src/main/java/com/strudel/alexeyool/strudel/MainActivity.java
@@ -3,23 +3,34 @@ package com.strudel.alexeyool.strudel;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.PopupMenu;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     Context mContext;
 
     ImageButton settingsActionBar;
+
+    public static ListView list;
+    public static ListAdapter listAdapter;
+    public static ArrayList<Bitmap> coversArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,33 @@ public class MainActivity extends AppCompatActivity {
 
         actionBarCreate();
 
+        downloadCoversToList();
+        createListView();
+
+
+    }
+
+    private void downloadCoversToList() {
+        createListView();
+        ArrayList<String> coversUrls = getCoversUrls();
+        new DownloadCovers(mContext, coversUrls).execute("");
+    }
+
+    private void createListView() {
+        list = (ListView) findViewById(R.id.AM_listview);
+        list.setOnItemClickListener(onItemClickListener);
+    }
+
+    AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+    };
+
+    private ArrayList<String> getCoversUrls() {
+        ArrayList<String> conversUrlArray = new ArrayList<String>();
+        return null;
     }
 
     private void actionBarCreate() {
@@ -81,3 +119,6 @@ public class MainActivity extends AppCompatActivity {
     };
 
 }
+
+
+

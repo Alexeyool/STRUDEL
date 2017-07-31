4338acbb342a4c99d08d6b7e5055399c91adb993
Alexeyool <Alexeymmy@gmail.com>
2017-07-31 18:13:45 +0300

    save covers images and pdf Filesto internalStorege pageView open pdf pages

--- app/src/main/java/com/strudel/alexeyool/strudel/MainActivity.java
+++ app/src/main/java/com/strudel/alexeyool/strudel/MainActivity.java
@@ -2,10 +2,14 @@ package com.strudel.alexeyool.strudel;
 
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,21 +20,32 @@ import android.view.View;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String COVERS_DIRECTORY = "coversDir";
+    public static final String MONTH = "month";
+    public static final String YEAR = "year";
+
     Context mContext;
 
     ImageButton settingsActionBar;
 
-    public static ListView list;
-    public static ListAdapter listAdapter;
-    public static ArrayList<Bitmap> coversArrayList;
+    ListView list;
+    ListAdapter listAdapter;
+    ArrayList<Cover> coversArrayList;
+
+    DownloadCovers downloadCovers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +55,18 @@ public class MainActivity extends AppCompatActivity {
         mContext = this;
 
         actionBarCreate();
-
-        downloadCoversToList();
         createListView();
-
+        downloadCoversToList();
 
     }
 
     private void downloadCoversToList() {
-        createListView();
-        ArrayList<String> coversUrls = getCoversUrls();
-        new DownloadCovers(mContext, coversUrls).execute("");
+        downloadCovers = (DownloadCovers) getLastNonConfigurationInstance();
+        if (downloadCovers == null) {
+            downloadCovers = new DownloadCovers(mContext);
+            downloadCovers.execute("");
+        }
+        downloadCovers.link(this);
     }
 
     private void createListView() {
@@ -61,14 +77,12 @@ public class MainActivity extends AppCompatActivity {
     AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+            Intent intent = new Intent(MainActivity.this, SliderActivity.class);
+            intent.putExtra(MONTH, coversArrayList.get(position).getMonth());
+            intent.putExtra(YEAR, coversArrayList.get(position).getYear());
+            startActivity(intent);
         }
     };
-
-    private ArrayList<String> getCoversUrls() {
-        ArrayList<String> conversUrlArray = new ArrayList<String>();
-        return null;
-    }
 
     private void actionBarCreate() {
         ActionBar mActionBar = getSupportActionBar();
@@ -118,7 +132,144 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-}
 
+    private static class DownloadCovers extends AsyncTask<String, Void, ArrayList<Cover>> {
+        Context mContext;
+        MainActivity activity;
+
+        public DownloadCovers(Context _mContext) {
+            mContext = _mContext;
+        }
+
+        void link(MainActivity act) {
+            activity = act;
+        }
+
+        void unLink() {
+            activity = null;
+        }
+
+
+        protected ArrayList<Cover> doInBackground(String... params) {
+            activity.coversArrayList = new ArrayList<>();
+            updateCoversFilesInInternalStorageFromUrls();
+            getImagesFromInternalStorageToArrayList();
+            return upend(activity.coversArrayList);
+        }
+
+        private void updateCoversFilesInInternalStorageFromUrls() {
+            Log.e("myy", "updateCoversFilesInInternalStorageFromUrls");
+            int i = 4;
+            int j = 2017;
+            while (i != 0) {
+                Cover cover = new Cover(i , j);
+                if (fileExist(cover.fileName)) {
+                    if (i < 12) {
+                        i++;
+                    } else {
+                        i = 1;
+                        j++;
+                    }
+                    activity.coversArrayList.add(cover);
+                }
+                else {
+                    try {
+                        InputStream inputStream = new java.net.URL(cover.getUrl()).openStream();
+                        Bitmap temp = BitmapFactory.decodeStream(inputStream);
+                        if (temp != null) {
+                            activity.coversArrayList.add(cover);
+                            saveToInternalStorage(cover, temp);
+                            if (i < 12) {
+                                i++;
+                            } else {
+                                i = 1;
+                                j++;
+                            }
+                        } else {
+                            i = 0;
+                        }
+                    } catch (Exception e) {
+                        Log.e("myy", e.getMessage());
+                        i = 0;
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        private boolean fileExist(String fname){
+            Log.e("myy", "fileExist");
+        //    "/data/user/0/com.strudel.alexeyool.strudel/app_coversDir"
+            ContextWrapper cw = new ContextWrapper(activity.getApplicationContext());
+            File directory = cw.getDir(COVERS_DIRECTORY, Context.MODE_PRIVATE);
+            File file = new File(directory, fname);
+            return file.exists();
+        }
+
+        private String saveToInternalStorage(Cover cover, Bitmap bitmapImage){
+            Log.e("myy", "saveToInternalStorage");
+//            File sdPath = Environment.get;
+//            sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+//            sdPath.mkdirs();
+
+            ContextWrapper cw = new ContextWrapper(activity.getApplicationContext());
+            File directory = cw.getDir(COVERS_DIRECTORY, Context.MODE_PRIVATE);
+            File mypath=new File(directory, cover.fileName);
+
+            FileOutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = new FileOutputStream(mypath);
+                bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return directory.getAbsolutePath();
+        }
+
+        private void getImagesFromInternalStorageToArrayList() {
+            Log.e("myy", "getImagesFromInternalStorageToArrayList");
+            int size = activity.coversArrayList.size();
+            for(int i=0; i<size; i++){
+                loadImageFromInternalStorage(activity.coversArrayList.get(i), i);
+            }
+        }
+
+        private void loadImageFromInternalStorage(Cover cover, int position) {
+            Log.e("myy", "loadImageFromInternalStorage" + position);
+            ContextWrapper cw = new ContextWrapper(activity.getApplicationContext());
+            File directory = cw.getDir(COVERS_DIRECTORY, Context.MODE_PRIVATE);
+            try {
+      //          "/data/user/0/com.strudel.alexeyool.strudel/app_coversDir"
+                File file = new File(directory, cover.fileName);
+                if(file.exists()) {
+                    activity.coversArrayList.get(position).addImage(BitmapFactory.decodeStream(new FileInputStream(file)));
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        protected void onPostExecute(ArrayList<Cover> result) {
+            activity.listAdapter = new ListAdapter(mContext, result);
+            activity.list.setAdapter(activity.listAdapter);
+        }
+
+        private ArrayList<Cover> upend(ArrayList<Cover> _temp){
+            activity.coversArrayList = new ArrayList<>();
+            for(int i=_temp.size()-1; i>-1; i--){
+                activity.coversArrayList.add(_temp.get(i));
+            }
+            return activity.coversArrayList;
+        }
+    }
+}
 
 
